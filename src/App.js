80c3f8408d29f3ef4d1534ab4d8e4bdcf40b1737
345257 <sbrrllss500@gmail.com>
2024-03-18 17:38:06 +0900

    ~9

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+const { Component } = require("react");
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  initialExpenses = [
+    { id: 1, charge: "렌트비", amount: 1600 }, 
+    { id: 2, charge: "교통비", amount: 400 }, 
+    { id: 3, charge: "식비", amount: 1200 }
+  ]
+  render() {
+    return (
+      <main className="main-container">
+        <h1>예산 계산기</h1>
+        <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+          <ExpenseForm/>
+        </div>
+        <div style={{ width: '100%', backgroundColor: 'white', padding: '1rem' }}>
+          <ExpenseList initialExpenses={this.initialExpenses}/>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'end', marginTop: '1rem' }}>
+          <p style={{ fontSize: '2rem' }}>
+            총지출: <span>원</span>
+          </p>
+        </div>
+      </main>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./ExpenseList.css";
+import ExpenseItem from "./ExpenseItem";
+import { MdDelete } from "react-icons/md";
+
+export class ExpenseList extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <ul className="list">
+          { this.props.initialExpenses.map(expense => {
+            return <ExpenseItem expense={expense} key={ expense.id }/>
+          }) }
+        </ul>
+        <button className="btn">
+          목록 지우기
+          <MdDelete className="btn-icon"/>
+        </button>
+      </React.Fragment>
+    )
+  }
+}
+
+export default ExpenseList
