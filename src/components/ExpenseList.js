ab0867c5806e3fe4565f7f3b13cef3633ca614ab
345257 <sbrrllss500@gmail.com>
2024-03-20 15:19:07 +0900

    ~9-3-3

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ExpenseList.css";
 import ExpenseItem from "./ExpenseItem";
 import { MdDelete } from "react-icons/md";
 
-export class ExpenseList extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <ul className="list">
-          { this.props.initialExpenses.map(expense => {
-            return <ExpenseItem expense={expense} key={ expense.id }/>
-          }) }
-        </ul>
-        <button className="btn">
-          목록 지우기
-          <MdDelete className="btn-icon"/>
-        </button>
-      </React.Fragment>
-    )
-  }
+const ExpenseList = ({handleDelete, initialExpenses}) => {
+  return (
+    <React.Fragment>
+      <ul className="list">
+        { initialExpenses.map(expense => {
+          return <ExpenseItem expense={expense} key={ expense.id } handleDelete={handleDelete}/>
+        }) }
+      </ul>
+      <button className="btn">
+        목록 지우기
+        <MdDelete className="btn-icon"/>
+      </button>
+    </React.Fragment>
+  )
 }
 
 export default ExpenseList
