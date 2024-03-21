d3f8345b5a54f63478a6494fc7e75815a13c01cf
345257 <sbrrllss500@gmail.com>
2024-03-21 16:32:00 +0900

    ~9-3-10

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -3,18 +3,20 @@ import "./ExpenseList.css";
 import ExpenseItem from "./ExpenseItem";
 import { MdDelete } from "react-icons/md";
 
-const ExpenseList = ({handleDelete, initialExpenses}) => {
+const ExpenseList = ({handleDelete, expenses, handleEdit, clearItems}) => {
   return (
     <React.Fragment>
       <ul className="list">
-        { initialExpenses.map(expense => {
-          return <ExpenseItem expense={expense} key={ expense.id } handleDelete={handleDelete}/>
+        { expenses.map(expense => {
+          return <ExpenseItem expense={expense} key={ expense.id } handleDelete={handleDelete} handleEdit={handleEdit}/>
         }) }
       </ul>
-      <button className="btn">
-        목록 지우기
-        <MdDelete className="btn-icon"/>
-      </button>
+      { expenses.length > 0 && (
+        <button className="btn" onClick={clearItems}>
+          목록 지우기
+          <MdDelete className="btn-icon"/>
+        </button>
+      )}
     </React.Fragment>
   )
 }
